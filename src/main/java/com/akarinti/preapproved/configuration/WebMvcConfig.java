4c60afe9651702bf75da00a869a39b17ceeda57d
Andrew Junior <andrew@akarinti.tech>
2021-07-06 15:16:07 +0700

    add cors allow all origin

--- src/main/java/com/akarinti/preapproved/configuration/WebMvcConfig.java
+++ src/main/java/com/akarinti/preapproved/configuration/WebMvcConfig.java
@@ -3,34 +3,24 @@ package com.akarinti.preapproved.configuration;
 import com.akarinti.preapproved.configuration.jwt.JwtAuthenticationEntryPoint;
 import com.akarinti.preapproved.configuration.jwt.JwtAuthenticationFilter;
 import com.akarinti.preapproved.configuration.jwt.SessionAuthenticationProvider;
-import com.akarinti.preapproved.service.SignInService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-//import com.akarinti.mypartner.utils.filter.LogFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebMvcConfig extends WebSecurityConfigurerAdapter {
+public class WebMvcConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Autowired
     private JwtAuthenticationEntryPoint unauthorizedHandler;
@@ -60,6 +50,11 @@ public class WebMvcConfig extends WebSecurityConfigurerAdapter {
                 .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
 }
