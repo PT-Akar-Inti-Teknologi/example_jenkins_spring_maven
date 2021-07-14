cc3d317a5965f4be9ef1507b2ab5069b296db2b7
Andrew Junior <andrew@akarinti.tech>
2021-07-14 14:21:43 +0700

    add aplikasi repository

---
+++ src/main/java/com/akarinti/preapproved/jpa/repository/AplikasiRepository.java
@@ -0,0 +1,10 @@
+package com.akarinti.preapproved.jpa.repository;
+
+import com.akarinti.preapproved.jpa.entity.Aplikasi;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AplikasiRepository extends JpaRepository<Aplikasi, Integer> {
+
+}
