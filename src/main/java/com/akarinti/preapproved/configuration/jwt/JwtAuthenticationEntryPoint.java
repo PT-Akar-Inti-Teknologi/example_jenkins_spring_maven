3f208a581299511b6f324d3aec2c9e51c85d83b1
Andrew Junior <andrew@akarinti.tech>
2021-07-23 09:42:01 +0700

    update unauthorized body response

--- src/main/java/com/akarinti/preapproved/configuration/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/akarinti/preapproved/configuration/jwt/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,9 @@
 package com.akarinti.preapproved.configuration.jwt;
 
+import com.akarinti.preapproved.dto.ResultDTO;
+import com.akarinti.preapproved.utils.exception.StatusCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -8,13 +12,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Serializable;
 
 @Component
+@Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        final ResultDTO resultDTO = new ResultDTO(StatusCode.UNAUTHORIZED, null);
+        ObjectMapper mapper = new ObjectMapper();
+        String responseMsg = mapper.writeValueAsString(resultDTO);
+
+        response.getWriter().write(responseMsg);
     }
 }
