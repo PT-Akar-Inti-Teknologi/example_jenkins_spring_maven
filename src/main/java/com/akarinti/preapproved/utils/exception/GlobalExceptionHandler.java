9235f66f07dfeb8ddc1e5239015c17e32c489b3a
Andrew Junior <andrew@akarinti.tech>
2021-07-23 09:57:31 +0700

    update

--- src/main/java/com/akarinti/preapproved/utils/exception/GlobalExceptionHandler.java
+++ src/main/java/com/akarinti/preapproved/utils/exception/GlobalExceptionHandler.java
@@ -8,9 +8,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,7 +20,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,33 +30,50 @@ import java.util.List;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<Object> customExceptionHandler(CustomException ex, WebRequest request) {
+        log.info("CustomException: "+ ex);
         final ResultDTO resultDTO = new ResultDTO(ex.getErrorMessage(), null);
-        return new ResponseEntity(resultDTO, new HttpHeaders(), ex.getStatus());
+        return new ResponseEntity<>(resultDTO, new HttpHeaders(), ex.getStatus());
     }
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handle(Exception ex, HttpServletRequest request, HttpServletResponse response) {
-        log.info("internal server error: "+ ex.getLocalizedMessage());
-        if (ex instanceof NullPointerException) {
-            final ResultDTO resultDTO = new ResultDTO(StatusCode.INVALID_ARGUMENT, ex.getMessage());
-            return new ResponseEntity(resultDTO, new HttpHeaders(), HttpStatus.BAD_REQUEST);
-        }
+    @ExceptionHandler({Exception.class, RuntimeException.class})
+    public ResponseEntity<Object> handle(HttpServletRequest req, Exception ex) {
+        log.info("internal server error: ");
+        log.info("ex: "+ ex.getClass());
+        log.error(ex.getLocalizedMessage(), ex);
+//        if (ex instanceof NullPointerException) {
+//            final ResultDTO resultDTO = new ResultDTO(StatusCode.INVALID_ARGUMENT, ex.getMessage());
+//            return new ResponseEntity(resultDTO, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
         final ResultDTO resultDTO = new ResultDTO(StatusCode.INTERNAL_SERVER_ERROR, ex.getMessage());
-        return new ResponseEntity(resultDTO, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(resultDTO, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
     @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    @NonNull
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(@NonNull HttpMessageNotReadableException ex, @NonNull HttpHeaders headers, @NonNull HttpStatus status, @NonNull WebRequest request) {
+        log.info("handleHttpMessageNotReadable: "+ ex);
         final ResultDTO resultDTO = new ResultDTO(StatusCode.INVALID_JSON, null);
-        return new ResponseEntity(resultDTO, headers, status);
+        return new ResponseEntity<>(resultDTO, headers, status);
     }
 
+
     @Override
+    @NonNull
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        final ResultDTO resultDTO = new ResultDTO(StatusCode.INVALID_ARGUMENT, body);
+        ResultDTO resultDTO;
+        log.info("exception: "+ ex);
+        log.info("body: "+ body);
+        log.info("status: "+ status);
+        if (ex.getClass().equals(HttpRequestMethodNotSupportedException.class)) {
+            resultDTO = new ResultDTO(StatusCode.METHOD_NOT_ALLOWED, body);
+        } else {
+            resultDTO = new ResultDTO(StatusCode.INVALID_ARGUMENT, body);
+        }
         logger.info("class: "+ ex.getClass().getName());
-        return new ResponseEntity(resultDTO, headers, status);
+        return new ResponseEntity<>(resultDTO, headers, status);
     }
     @Override
+    @NonNull
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        log.info("MethodArgumentNotValidException: "+ ex);
         final List<HashMap<String, String>> errors = new ArrayList<>();
         for (final FieldError error : ex.getBindingResult().getFieldErrors()) {
             HashMap<String, String> fieldError = new HashMap<>();
@@ -72,6 +90,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             errors.add(fieldError);
         }
         final ResultDTO resultDTO = new ResultDTO(StatusCode.INVALID_ARGUMENT, errors);
-        return new ResponseEntity(resultDTO, headers, status);
+        return new ResponseEntity<>(resultDTO, headers, status);
     }
 }
