52c6d034791a247e6a475002cab091a36c90d25a
Andrew Junior <andrew@akarinti.tech>
2021-07-27 16:51:26 +0700

    update application controller, add status filter for get list and add file name parameter for download image

--- src/main/java/com/akarinti/preapproved/controller/AplikasiController.java
+++ src/main/java/com/akarinti/preapproved/controller/AplikasiController.java
@@ -7,14 +7,20 @@ import com.akarinti.preapproved.dto.rumahsaya.RumahSayaResponseDTO;
 import com.akarinti.preapproved.service.AplikasiService;
 import com.akarinti.preapproved.utils.exception.StatusCode;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -34,9 +40,42 @@ public class AplikasiController {
 
     @GetMapping(value = "/rumahsaya",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResultDTO> getData(@RequestParam(required = false) String search, @PageableDefault(size = 20) Pageable pageable) {
-        ResultDTO response = aplikasiService.getData(search, pageable);
+    public ResponseEntity<ResultDTO> getData(@RequestParam(required = false) String search,
+                                             @RequestParam(required = false) String status,
+                                             @PageableDefault(size = 20) Pageable pageable) {
+        ResultDTO response = aplikasiService.getData(search, status, pageable);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(value = "/image/download/{secureIdAplikasi}/{fileName}")
+    public ResponseEntity<ResultDTO> getImage(HttpServletResponse response,
+                             @PathVariable("secureIdAplikasi") String secureIdAplikasi,
+                             @PathVariable("fileName") String fileName
+    ) throws IOException {
+        Map<String, Object> map = aplikasiService.downloadImage(secureIdAplikasi, fileName);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+
+        ResultDTO resultDTO = new ResultDTO(StatusCode.OK.message(), map);
+        ResponseEntity<ResultDTO> responseEntity = new ResponseEntity<>(resultDTO, headers, HttpStatus.OK);
+        return responseEntity;
+    }
+
+    @GetMapping(value = "/file/download/{secureIdAplikasi}/{fileName}")
+    public void downloadFile(HttpServletResponse response,
+                             @PathVariable("secureIdAplikasi") String secureIdAplikasi,
+                             @PathVariable("fileName") String fileName
+    ) throws IOException {
+        Map<String, Object> map = aplikasiService.downloadImage(secureIdAplikasi, fileName);
+        File file = (File) map.get("file");
+
+        InputStream inputStream = new FileInputStream(file);
+        response.setHeader("Content-Disposition", "filename=" + file.getName());
+        IOUtils.copy(inputStream, response.getOutputStream());
+
+        response.flushBuffer();
+        inputStream.close();
     }
 
 }
