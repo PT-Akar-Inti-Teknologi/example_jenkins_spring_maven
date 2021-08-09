6d2f9a6d0dbd611c68ae0dc51a5fdbbdbca86784
Andrew Junior <andrew@akarinti.tech>
2021-08-09 16:59:19 +0700

    add trigger retry

--- src/main/java/com/akarinti/preapproved/controller/TriggerController.java
+++ src/main/java/com/akarinti/preapproved/controller/TriggerController.java
@@ -1,6 +1,8 @@
 package com.akarinti.preapproved.controller;
 
+import com.akarinti.preapproved.dto.DataResponseDTO;
 import com.akarinti.preapproved.dto.ResultDTO;
+import com.akarinti.preapproved.service.AplikasiService;
 import com.akarinti.preapproved.service.DataManagementService;
 import com.akarinti.preapproved.utils.exception.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +17,21 @@ public class TriggerController {
     @Autowired
     DataManagementService dataManagementService;
 
+    @Autowired
+    AplikasiService aplikasiService;
+
     @GetMapping(value = "/sync-holiday",
             produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<ResultDTO> syncHoliday(){
         Boolean response = dataManagementService.syncHoliday();
+        return ResponseEntity.ok(new ResultDTO(StatusCode.OK.message(), response));
+    }
+
+    @PostMapping(value = "/retry-error-cbas",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResultDTO> retryError() {
+        DataResponseDTO response = aplikasiService.retryErrorCBAS();
         return ResponseEntity.ok(new ResultDTO(StatusCode.OK.message(), response));
     }
 
